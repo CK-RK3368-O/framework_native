6b2e3e76274c7bd9b4658cd205124766394379af
chenzhen <chenzhen@rock-chips.com>
2017-07-11 15:17:42 +0800

    rk_ext: HWrotation: inputflinger: add another InputReader::copyFrom.
    Commit from: bb1f7f6.
    
    Change-Id: Ib4553cf882c28dac0dd44aa91999a11738a5f1d1
    Signed-off-by: Steven Chen <cw@rock-chips.com>

--- include/input/DisplayViewport.h
+++ include/input/DisplayViewport.h
@@ -87,6 +87,20 @@ struct DisplayViewport {
         deviceHeight = height;
         uniqueId.clear();
     }
+    void copyFrom(const DisplayViewport& other) {
+        displayId = other.displayId;
+        orientation = other.orientation;
+        logicalLeft = other.logicalLeft;
+        logicalTop = other.logicalTop;
+        logicalRight = other.logicalRight;
+        logicalBottom = other.logicalBottom;
+        physicalLeft = other.physicalLeft;
+        physicalTop = other.physicalTop;
+        physicalRight = other.physicalRight;
+        physicalBottom = other.physicalBottom;
+        deviceWidth = other.deviceWidth;
+        deviceHeight = other.deviceHeight;
+    }
 };
 
 /**
